50083050129e80ebab1578df83296babd36c9251
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2025-09-18 15:09:46 -0700

    Secure API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
 
@@ -27,6 +28,8 @@ app.use((error, req, res, next) => {
 });
 
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
+
+app.use(securityMiddleware)
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
